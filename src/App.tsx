a4aa89435f19eca5661a3b3f9a73bbd57554ea90
hgko1207 <khkkhk1207@gmail.com>
2020-02-16 01:14:42 +0900

    React State and Tyepscript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div className="App"></div>;
-};
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0
+  };
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        {counter}
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+
+  add = () => {
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1
+      };
+    });
+  };
+}
 
 export default App;
